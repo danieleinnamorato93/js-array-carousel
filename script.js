32b045d2de19c9ad29f69ac7ee648f87c35275e6
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-20 18:23:05 +0200

    Esercizio terminato

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
 const carouselGallery = document.querySelector('.gallery');
+const thumbnailsContainer = document.querySelector('.thumbnails');
 
 //Richiamo sorgenti immagine
 const sources = ['img/01.webp', 'img/02.webp' , 'img/03.webp' , 
@@ -10,23 +11,64 @@ const sources = ['img/01.webp', 'img/02.webp' , 'img/03.webp' ,
 
 // Genero le immagini col ciclo for
 let imgs = '';
+let thumbnails = '';
 for(let i = 0; i < sources.length; i++) {
    imgs += `<img alt="videogame-${i+1}" src="${sources[i]}">`;
+   thumbnails += `<img alt="thumbnail-${i + 1}" src="${sources[i]}">`;
 }
-//inserisco le immagini nel DOM
+//inserisco le immagini e miniature nel DOM
 carouselGallery.innerHTML = imgs;
-const images = document.querySelectorAll('#carousel img')
+thumbnailsContainer.innerHTML = thumbnails;
 
-//Mostro la prima immagine
+const images = document.querySelectorAll('.gallery img');
+const thumbnailsImages = document.querySelectorAll('.thumbnails img');
+
+
+
+//Mostro la prima immagine e la sua miniatura
 let currentActiveIndex = 0;
 images[currentActiveIndex].classList.add('active');
+thumbnailsImages[currentActiveIndex].classList.add('active');
+
 
 //Aggiungo gli eventi per i pusanti
 nextButton.addEventListener('click',  function() {
-images[currentActiveIndex].classList.remove('active');
+    images[currentActiveIndex].classList.remove('active');
+    thumbnailsImages[currentActiveIndex].classList.remove('active');
+
 currentActiveIndex++;
+
 if (currentActiveIndex >= images.length) {
     currentActiveIndex = 0;
 }
 images[currentActiveIndex].classList.add('active');
+thumbnailsImages[currentActiveIndex].classList.add('active');
 });
+
+prevButton.addEventListener('click', function() {
+    images[currentActiveIndex].classList.remove('active');
+    thumbnailsImages[currentActiveIndex].classList.remove('active');
+
+    currentActiveIndex--;
+    
+    if (currentActiveIndex < 0) {
+        currentActiveIndex = images.length - 1;
+    }
+    images[currentActiveIndex].classList.add('active');
+    thumbnailsImages[currentActiveIndex].classList.add('active');
+});
+
+    for (let i = 0; i < thumbnailsImages.length; i++) {
+        const thumbnail = thumbnailsImages[i];
+
+        thumbnail.addEventListener('click', function() {
+
+        images[currentActiveIndex].classList.remove('active');
+        thumbnailsImages[currentActiveIndex].classList.remove('active');
+
+        currentActiveIndex = i;
+        
+        images[currentActiveIndex].classList.add('active');
+        thumbnailsImages[currentActiveIndex].classList.add('active');
+    });
+}
