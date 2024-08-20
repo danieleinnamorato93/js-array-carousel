ebb8a72ea3ddd687d31f0136ab1ea937554b4f34
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-20 16:21:43 +0200

    preparazione

---
+++ script.js
@@ -0,0 +1,8 @@
+//Preparazione:Richiamo elementi del Dom
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const images = document.querySelectorAll ('#carousel img');
+
+//Richiamo sorgenti immagine
+const sources = ['img/01.webp', 'img/02.webp' , 'img/03.webp' , 
+    'img/04.webp', 'img/05.webp'];
