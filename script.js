b5fc2fc572579bb426adec4849bb4966b3045c58
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-20 16:41:19 +0200

    style/ciclofor

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-//Preparazione:Richiamo elementi del Dom
+//Preparazione: Richiamo elementi del Dom
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
-const images = document.querySelectorAll ('#carousel img');
+const carouselGallery = document.querySelectorAll ('.gallery');
 
 //Richiamo sorgenti immagine
 const sources = ['img/01.webp', 'img/02.webp' , 'img/03.webp' , 
     'img/04.webp', 'img/05.webp'];
+
+
+// Genero le immagini col ciclo for
+for(let i = 0; i < sources.length; i++){
+    const image = document.createElement('img');
+    image.src = sources[i];
+    carouselGallery.appendChild(image);
+}
